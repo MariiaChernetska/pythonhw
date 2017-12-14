eb7e90efffe214e438b26e797c41eb7716bb4586
mchernetska <mariachernetska@outlook.com>
2017-12-14 13:31:53 +0200

    4.1

--- 4/4.1.py
+++ 4/4.1.py
@@ -1,10 +1,42 @@
-def rec(elem, prev):
-    if elem==34: return 0
-    if elem==0: print(elem)
-    print(elem+prev)
+#solution using recursion
+def rec(elem, prev, index, counter):
+
     newprev = elem
     newelem = elem+prev
-    rec(newelem, newprev)
+
+    if index == counter:
+        print("Element " + str(index) + " equals " + str(prev))
+        return 0
+
+    counter+=1
+    rec(newelem, newprev, index, counter)
+
+#solution using loop
+def loop(index):
+    elem = 1
+    prev = 0
+
+    counter = 0
+    while index != counter:
+       # print(prev)
+        buf = elem
+        elem=elem+prev
+        prev = buf
+        counter+=1
+    print("Element " +str(index)+" equals "+str(prev))
+
+index = input("Enter element num ")
+
+way = input("Use recursion - 1, loop - 2 ")
+if int(way)==1:
+    rec(1,0, int(index), 0)
+else: loop(int(index))
 
 
-rec(0,1)
+
+
+
+
+
+
+
