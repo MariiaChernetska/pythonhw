73fa96b56d8ee6c53ea13f6c3337e931778db752
mchernetska <mariachernetska@outlook.com>
2017-12-21 14:05:04 +0200

    7 completed

--- 7/7.2.py
+++ 7/7.2.py
@@ -1,10 +1,13 @@
 import argparse
 import os
-
+import time
+import datetime
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", type=str,
                     help="the folder path", default=".")
 parser.add_argument("-type", type=str,
+                    help="file type filter", default="")
+parser.add_argument("-date", type=str,
                     help="file type filter", default="")
 parser.add_argument("--sub",
                     help="show files in subfolders", action="store_true", default=False)
@@ -16,8 +19,21 @@ args = parser.parse_args()
 
 print(args)
 
+def check_by_ext(files_list, ext):
+    if len(ext)>0:
+        return list([file for file in files_list if file.endswith(ext)])
+    else:
+        return files_list
 
-def get_files_and_directories(path, extension="", show_folders=False, show_subfolders_content=False):
+def check_by_date(files_list, date_mod, root_dir):
+    if len(date_mod)>0:
+        date_c = datetime.datetime.strptime(date_mod, "%d/%m/%Y")
+        return list([file for file in files_list if datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(root_dir,file)))<=date_c])
+    else:
+        return files_list
+
+
+def get_files_and_directories(path, extension, date_modified, show_folders=False, show_subfolders_content=False):
 
     if show_subfolders_content:
         for rootdir, dirs, files in os.walk(path):
@@ -26,13 +42,12 @@ def get_files_and_directories(path, extension="", show_folders=False, show_subfo
                 if(rootdir == path):
                     print(os.path.split(rootdir)[1])
                 else: print(str(rootdir).replace(path, ''))
+            files_res =  check_by_ext(check_by_date(files, date_modified, rootdir), extension)
+            for file in files_res:
+                    print("\t--" + file)
 
-            for file in files:
-
-                if len(extension ) > 0:
-                    if file.endswith(extension): print("\t--"+file)
-                else: print("\t--"+file)
     else:
+
         content = os.listdir(path)
 
         files = list([file for file in content if os.path.isfile(os.path.join(path,file))])
@@ -42,16 +57,11 @@ def get_files_and_directories(path, extension="", show_folders=False, show_subfo
                 if os.path.isdir(os.path.join(path,item)):
                     print(item)
 
-
-        for file in files:
-
-            if len(extension ) > 0:
-                if file.endswith(extension): print(file)
-
-            else:
-                print(file)
+        files_res = check_by_ext(check_by_date(files, date_modified, path), extension)
+        for file in files_res:
+            print("\t--" + file)
 
 
-res = get_files_and_directories(args.d, args.type, args.dirs, args.sub)
+res = get_files_and_directories(args.d, args.type, args.date, args.dirs, args.sub)
 
 
